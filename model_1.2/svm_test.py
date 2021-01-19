06b9d5f9aafdb8b6cac802d3beb3def695e578a2
Ryan Saweczko <ryansaweczko@kingsds.network>
2021-01-19 14:49:15 -0500

    small changes to svm

--- model_1.2/svm_test.py
+++ model_1.2/svm_test.py
@@ -1,10 +1,6 @@
 import numpy as np
 import pandas as pd
 import os
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow import feature_column
-from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
@@ -16,12 +12,14 @@ dirname = os.path.dirname(__file__)
 data = pd.read_csv(os.path.join(dirname,"../data/Model_1.2Dataset.csv"), index_col="Unnamed: 0")
 data = data.drop("URL", axis=1)
 data = data.drop("Label",axis=1)
+
+data = data.sample(frac=0.05)
+
 labels = data['Target']
 data = data.drop("Target", axis=1)
 
 data = data.to_numpy()
 labels = labels.to_numpy()
-
 
 # split dataset
 trainX, testX, trainY, testY = train_test_split(data, labels, test_size=0.3)
@@ -29,7 +27,9 @@ trainX, testX, trainY, testY = train_test_split(data, labels, test_size=0.3)
 
 model = svm.SVC(kernel= "linear")
 
+print("model training started")
 model.fit(trainX, trainY)
+print("predicting...")
 pred = model.predict(testX)
 
 print("Accuracy", metrics.accuracy_score(testY, pred))
