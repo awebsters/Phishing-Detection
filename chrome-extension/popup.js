c24439543bc03863e74158b8422e5fc52c00119d
Ryan <ryansaweczko@kingsds.network>
2021-02-12 18:36:31 -0500

    server stuff using the popup, WIP

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,8 +1,20 @@
 var bkg = chrome.extension.getBackgroundPage();
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
+chrome.runtime.onMessage.addListener( async function(request, sender) {
   if (request.action == "scrapeLinks") {
     bkg.log(request.source);
+  let response;
+  try {
+    response = await fetch('18.216.209.229/8081/test', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: response.source.join(", "),
+    })
+    } catch (error) {
+      console.error(error);
+    }
   }
 });
 
