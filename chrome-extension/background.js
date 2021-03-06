9c0a4df1f5d498b6e1c37cb2ae1299c3c9e458ea
awebsters <17aw43@queensu.ca>
2021-03-05 23:16:29 -0500

    not testing websites that are on the same domain name as the current

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,7 +1,6 @@
 const SERVER_URL = "http://3.139.57.29/";
 
-
-var RUN = localStorage.getItem("autoRun") == 'true';
+var RUN = localStorage.getItem("autoRun") == "true";
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action == "scrapeLinks") {
@@ -14,26 +13,33 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(urls),
-      }).then(response => {
-        return response.json();
-      }).then(data => {
-        chrome.storage.local.set({"data":data})
-        chrome.tabs.executeScript(null, {
-          file: "injectWarnings.js",
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          chrome.storage.local.set({ data: data });
+          chrome.tabs.executeScript(null, {
+            file: "injectWarnings.js",
+          });
         });
-      }); 
     } catch (error) {
       console.error(error);
     }
   }
 });
 
+/*
+  On a tab update, rescan if its the active tab
+*/
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if ((changeInfo.status == 'complete') && tab.active && RUN){
-    if (tab.url.startsWith("http")){
+  if (changeInfo.status == "complete" && tab.active && RUN) {
+    // Only scan website tabs (not for example, setting tabs)
+    if (tab.url.startsWith("http")) {
       chrome.tabs.executeScript(null, {
         file: "getPagesSource.js",
       });
     }
   }
-})
+});
--- chrome-extension/getPagesSource.js
+++ chrome-extension/getPagesSource.js
@@ -1,14 +1,29 @@
+function getLinks() {
+  let tabHost = window.location.hostname.split(".").slice(-2);
+  let tabDomainName = tabHost[0] + "." + tabHost[1];
 
-function getLinks(document_root) {
-  let aTags = []
-  let urls = document.querySelectorAll('a')
-  for (url in urls){
-    aTags.push(urls[url].href)
+  let aTags = [];
+  let anchors = document.querySelectorAll("a");
+  for (anchor in anchors) {
+    // Lets filter out links on the same domain as the current page
+    // Websites lke gmail have navigation that links to gmail (no point in running these)
+    let url = anchors[anchor].href;
+
+    try {
+      let host = new URL(url).hostname.split(".").slice(-2);
+      var domainName = host[0] + "." + host[1];
+    } catch (error) {
+      continue;
+    }
+    console.log(domainName);
+    if (tabDomainName != domainName) {
+      aTags.push(url);
+    }
   }
-  return aTags
+  return aTags;
 }
 
 chrome.runtime.sendMessage({
   action: "scrapeLinks",
-  source: getLinks(document)
-})
+  source: getLinks(),
+});
